7375d92a2066786a1235d2b7ffc8086c42d8a0ca
Sahrul gunawan <sahrulcc07@gmail.com>
2025-03-19 14:26:21 +0700

    Delete P04_Fungsi_Eksepsi directory

--- P04_Fungsi_Eksepsi/ex1_fungsi_math.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:math';
-
-void main(List<String> args) {
-  double a;
-  a = sqrt(25.0);
-  print(a);
-  print(pow(a, 4));
-}
--- P04_Fungsi_Eksepsi/ex2_fungsi_return_null.dart
+++
@@ -1,8 +0,0 @@
-echo(dynamic value) {
-  print(value.toString());
-}
-
-main(List<String> args) {
-  var a = echo('Dart');
-  print('Nilai a: $a');
-}
